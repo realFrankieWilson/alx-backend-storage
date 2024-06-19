f52b7946816e7bac3a10f93591be0a0e8c89faa2
realFrankieWilson <frankuwill101@gmail.com>
2024-06-20 00:21:20 +0100

    hot fix

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -4,5 +4,5 @@ SELECT band_name,
   FLOOR(2022 - formed) AS lifespan
 FROM metal_bands
 WHERE split is NULL OR split > 2022
-  AND FIND_IN_SET('Glam rock', style) > 0
+  AND FIND_IN_SET('Glam rock', style) = 1
 ORDER BY lifespan DESC;
