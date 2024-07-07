6770c886a2b4def6cd2d16dbed3930028965e869
realFrankieWilson <frankuwill101@gmail.com>
2024-07-07 05:14:33 +0100

    modified excersie

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -7,7 +7,7 @@ private variable.
 
 import redis
 import uuid
-from typing import Union
+from typing import Callable, Optional, Union
 
 
 class Cache:
@@ -26,5 +26,28 @@ class Cache:
         returns a string.
         """
         key = str(uuid.uuid4())
-        self._redis.set(key, data)
+        if isinstance(data, str):
+            self._redis.set(key, data.encode("utf-8"))
+        else:
+            self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn:
+            Optional[Callable[[bytes], any]] = None) -> any:
+        """A method that convert data back to desired format"""
+
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn is None:
+            return data
+        return fn(data)
+
+    def get_str(self, key: str) -> str:
+        """Automatically parametrize Cache.get"""
+
+        return self.get(key, lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> int:
+        """Automatically parametrize Cache.get"""
+        return self.get(key, int)
